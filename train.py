5a69b2ebf2717483df6a385f9873bcdae8fdb7cb
smallflyfly <452639095@qq.com>
2019-12-16 16:31:51 +0800

    modify train.py

--- train.py
+++ train.py
@@ -8,7 +8,7 @@ import utils
 
 def train():
     mydataset =  MyDatasset('./dataset/PETA/')
-    dataloader = DataLoader(mydataset, batch_size=64, shuffle=True)
+    dataloader = DataLoader(mydataset, batch_size=32, shuffle=True)
     # print(len(dataloader))
     # print(type(dataloader))
     # net = DenseNet121()
@@ -31,7 +31,7 @@ def train():
     all_count = 0
     writer = tb.SummaryWriter()
     for epoch in range(num_epochs):
-        lr_scheduler.step()
+
         print('Epoch {} / {}'.format(epoch+1, num_epochs))
         count_epoch = 0
         # for index, data in enumerate(dataloader):
@@ -46,30 +46,28 @@ def train():
             # print(im)
             optimizer.zero_grad()
             out1, out2, out3, out4, out5 = net(im)
-            loss1 = loss_func_CEloss(out1, label[:, 0])
-            loss2 = loss_func_CEloss(out2, label[:, 1])
-            loss3 = loss_func_CEloss(out3, label[:, 2])
+            # loss1 = loss_func_CEloss(out1, label[:, 0])
+            # loss2 = loss_func_CEloss(out2, label[:, 1])
+            # loss3 = loss_func_CEloss(out3, label[:, 2])
             loss4 = loss_func_CEloss(out4, label[:, 3])
-            loss5 = loss_func_CEloss(out5, label[:, 4])
+            # loss5 = loss_func_CEloss(out5, label[:, 4])
             # loss = loss1 + loss2 + loss3 + loss4 + loss5
             loss = loss4
             loss.backward()
             optimizer.step()
-            writer.add_scalar('loss',loss,count_epoch)
+            lr_scheduler.step()
+            writer.add_scalar('loss',loss, all_count)
             count_epoch += 1
 
-            if all_count % 10 == 0:
-                print('{} / {} ------>loss {}'.format(all_count, len(dataloader), loss))
+            if count_epoch % 10 == 0 or (count_epoch+1)==len(dataloader):
+                print('{} / {} ------>loss {}'.format(count_epoch, len(dataloader), loss))
                 # print('----------->loss1 {}'.format(loss1))
                 # print('----------->loss2 {}'.format(loss2))
                 # print('----------->loss3 {}'.format(loss3))
-                print('----------->loss4 {}'.format(loss4))
+                # print('----------->loss4 {}'.format(loss4))
                 # print('----------->loss5 {}'.format(loss5))
         if (epoch+1) % 10 == 10:
             utils.save_network(net, epoch+1)
-        
-        if (epoch+1) % 10 == 0:
-            optimizer
     
     writer.close()
 
