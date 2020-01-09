a492450c9dd692975e6062651853e1f614a51b6b
Fangpf <fangpf5@chinaunicom.cn>
2020-01-09 14:06:24 +0800

    torch.cat add ()

--- networks/myresnet50.py
+++ networks/myresnet50.py
@@ -231,11 +231,11 @@ class ResNet50(ResNet):
         state_dict['fc1.weight'] = state_dict['fc.weight']
         state_dict['fc1.bias'] = state_dict['fc.bias']
 
-        state_dict['fc2.weight'] = torch.cat(state_dict['fc.weight'], state_dict['fc.weight'][0,:], dim=0)
-        state_dict['fc2.bias'] = torch.cat(state_dict['fc.bias'], state_dict['fc.bias'][0, :], dim=0)
+        state_dict['fc2.weight'] = torch.cat((state_dict['fc.weight'], state_dict['fc.weight'][0,:]), dim=0)
+        state_dict['fc2.bias'] = torch.cat((state_dict['fc.bias'], state_dict['fc.bias'][0, :]), dim=0)
 
-        state_dict['fc3.weight'] = torch.cat(state_dict['fc.weight'], state_dict['fc.weight'], dim=0)
-        state_dict['fc3.bias'] = torch.cat(state_dict['fc.bias'], state_dict['fc.bias'], dim=0)
+        state_dict['fc3.weight'] = torch.cat((state_dict['fc.weight'], state_dict['fc.weight']), dim=0)
+        state_dict['fc3.bias'] = torch.cat((state_dict['fc.bias'], state_dict['fc.bias']), dim=0)
 
 
         nn.Module.load_state_dict(self, {k: state_dict[k] for k in list(state_dict)})
